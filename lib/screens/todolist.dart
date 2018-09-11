8a360a9c6588052273e9869d72d133b3a6c5d506
ronek22 <jronek3010@gmail.com>
2018-09-11 12:34:07 +0200

    New feature added: Swipe to remove

--- lib/screens/todolist.dart
+++ lib/screens/todolist.dart
@@ -11,7 +11,6 @@ class TodoList extends StatefulWidget {
 class TodoListState extends State {
   DbHelper helper = DbHelper();
   List<Todo> todos;
-  int count = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +26,7 @@ class TodoListState extends State {
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
-            navigateToDetail(Todo('',3,''));
+            navigateToDetail(Todo('', 3, ''));
           },
           tooltip: "Add new Todo",
           child: new Icon(
@@ -41,87 +40,119 @@ class TodoListState extends State {
   ListView todoListItems() {
     return ListView.builder(
         padding: EdgeInsets.only(top: 10.0),
-        itemCount: count,
+        itemCount: todos.length,
         itemBuilder: (BuildContext context, int position) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 2.0),
-            child: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromARGB(255, 235, 235, 243),
-                      blurRadius: 10.0,
-                      spreadRadius: -9.0,
-                      offset: Offset(0.0, 7.0)),
-                ],
-              ),
-              child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  color: Colors.white,
-                  elevation: 0.0,
-                  child: ListTile(
-                    // TODO: Find the way to align this avatar to top left corner
-                    leading: Padding(
-                      padding: const EdgeInsets.only(left: 17.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          CircleAvatar(
-                            radius: 7.0,
-                            backgroundColor: getColor(this.todos[position].priority),
-                          ),
-                          SizedBox(
-                            height: 57.0,
-                          ),
-                        ],
-                      ),
-                    ),
+        return Container(
+           padding: EdgeInsets.only(bottom: 2.0),
+           decoration: BoxDecoration(
+             boxShadow: [
+               BoxShadow(
+                   color: Color.fromARGB(255, 235, 235, 243),
+                   blurRadius: 10.0,
+                   spreadRadius: -9.0,
+                   offset: Offset(0.0, 7.0)),
+             ],
+           ),
+           child: Dismissible(
+             direction: DismissDirection.endToStart,
+             background: Container(
+               alignment: Alignment.centerRight,
+               padding: EdgeInsets.only(right: 50.0),
+               decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(10.0),
+                   color: Colors.red),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.center,
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: <Widget>[
+                       Icon(Icons.delete_sweep, color: Colors.white),
+                       Text("remove", style: TextStyle(color: Colors.white),)
+                     ],
+                   )
+             ),
+             key: Key(todos[position].id.toString()),
+             onDismissed: (direction) {
+               int id = todos[position].id;
+               String title = todos[position].title.toString();
+               setState(() {
+                 todos.removeAt(position);
+                 helper.deleteTodo(id);
+                 getData();
+               });
 
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 10.0),
-                      child: Text(
-                        this.todos[position].title,
-                        style: TextStyle(
-                            fontSize: 15.0, fontWeight: FontWeight.w800),
-                      ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          this.todos[position].description,
-                          style: TextStyle(
-                              fontSize: 13.0,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black38),
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ), // it smell
-                        Text(
-                          'Created at ' +
-                              this.todos[position].date,
-                          style: TextStyle(
-                              fontSize: 11.0,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black38),
-                        ),
-                      ],
-                    ),
-                    contentPadding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 20.0),
-                    // contentPadding: EdgeInsets.zero,
-                    isThreeLine: true,
-                    onTap: () {
-                      debugPrint(
-                          "Tapped on " + this.todos[position].id.toString());
-                      navigateToDetail(this.todos[position]);
-                    },
-                  )),
-            ),
-          );
+               Scaffold.of(context).showSnackBar(SnackBar(
+                 content:
+                     Text("Todo: $title removed"),
+               ));
+             },
+             child: Card(
+                 shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(10.0)),
+                 color: Colors.white,
+                 elevation: 0.0,
+                 child: ListTile(
+                   // TODO: Find the way to align this avatar to top left corner
+                   leading: Padding(
+                     padding: const EdgeInsets.only(left: 17.0),
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.center,
+                       children: <Widget>[
+                         CircleAvatar(
+                           radius: 7.0,
+                           backgroundColor:
+                               getColor(todos[position].priority),
+                         ),
+                         SizedBox(
+                           height: 57.0,
+                         ),
+                       ],
+                     ),
+                   ),
+
+                   title: Padding(
+                     padding: const EdgeInsets.only(bottom: 10.0),
+                     child: Text(
+                       todos[position].title,
+                       style: TextStyle(
+                           fontSize: 15.0, fontWeight: FontWeight.w800),
+                     ),
+                   ),
+                   subtitle: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: <Widget>[
+                       Text(
+                         todos[position].description,
+                         style: TextStyle(
+                             fontSize: 13.0,
+                             fontWeight: FontWeight.w600,
+                             color: Colors.black38),
+                       ),
+                       SizedBox(
+                         height: 15.0,
+                       ), // it smell
+                       Text(
+                         'Created at ' + todos[position].date,
+                         style: TextStyle(
+                             fontSize: 11.0,
+                             fontWeight: FontWeight.w600,
+                             color: Colors.black38),
+                       ),
+                     ],
+                   ),
+                   contentPadding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 20.0),
+                   // contentPadding: EdgeInsets.zero,
+                   isThreeLine: true,
+                   onTap: () {
+                     debugPrint(
+                         "Tapped on " + todos[position].id.toString());
+                     navigateToDetail(todos[position]);
+                   },
+                 )),
+           ),
+         );
         });
   }
+
 
   void getData() {
     final dbFuture = helper.initalizeDb();
@@ -129,22 +160,20 @@ class TodoListState extends State {
       final todosFuture = helper.getTodos();
       todosFuture.then((result) {
         List<Todo> todoList = List<Todo>();
-        count = result.length;
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < result.length; i++) {
           todoList.add(Todo.fromObject(result[i]));
           debugPrint(todoList[i].title);
         }
         setState(() {
           todos = todoList;
-          count = count;
         });
-        debugPrint("Items: " + count.toString());
+        debugPrint("Items: " + todos.length.toString());
       });
     });
   }
 
   Color getColor(int priority) {
-    switch(priority){
+    switch (priority) {
       case 1:
         return Colors.red;
         break;
@@ -161,14 +190,13 @@ class TodoListState extends State {
   }
 
   void navigateToDetail(Todo todo) async {
-    bool result = await Navigator.push(context, 
+    bool result = await Navigator.push(
+      context,
       MaterialPageRoute(builder: (context) => TodoDetail(todo)),
     );
 
-    if(result == true){
+    if (result == true) {
       getData();
     }
-    
   }
-
 }
